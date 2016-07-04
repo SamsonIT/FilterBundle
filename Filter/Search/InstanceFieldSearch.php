7d73b5b85cbf2e26658df6f5cdff222dbb99103c
Erik Trapman <erik@samson-it.nl>
2016-07-04 11:20:11 +0200

    removed failing use-stmt

--- Filter/Search/InstanceFieldSearch.php
+++ Filter/Search/InstanceFieldSearch.php
@@ -4,8 +4,6 @@ namespace Samson\Bundle\FilterBundle\Filter\Search;
 
 use Samson\Bundle\FilterBundle\Filter\FieldSearch;
 
-use Samson\Bundle\FilterBundle\Filter\Search\String;
-
 /**
  * @Annotation
  */
