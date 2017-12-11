5eb0766f2cfc42df891fb9154efb01f9336278c2
jur <jelle@samson-it.nl>
2017-12-11 17:07:15 +0100

    Fix: instanceof array check

--- Filter/Search/ChoiceListFilter.php
+++ Filter/Search/ChoiceListFilter.php
@@ -2,6 +2,7 @@
 
 namespace Samson\Bundle\FilterBundle\Filter\Search;
 
+use Doctrine\Entity;
 use Samson\Bundle\FilterBundle\Filter\FieldSearch;
 use Doctrine\ORM\Query\Expr;
 use Samson\Bundle\FilterBundle\Filter\FieldFilter;
@@ -20,8 +21,10 @@ class ChoiceListFilter extends FieldFilter
             return null;
         }
         
-        if( $value instanceof \Doctrine\Common\Collections\Collection ){
+        if ($value instanceof \Doctrine\Common\Collections\Collection ){
             $value = $value->toArray();
+        } else if (!is_array($value)) {
+            $value = [$value];
         }
         
         return array(new Expr\Func($field.' IN', ':'.$parameterName), array($parameterName => array_values($value)));
